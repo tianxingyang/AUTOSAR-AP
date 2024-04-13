e1c335c0b365cd7a0a2f464324d2118d58aa2700
vitoyang <1586608030@qq.com>
2024-04-13 23:01:26 +0800

    implement variant

---
+++ include/public/ara/core/variant.h
@@ -0,0 +1,22 @@
+#ifndef VITO_AP_VARIANT_H_
+#define VITO_AP_VARIANT_H_
+
+#include <variant>
+
+namespace ara::core {
+/// @brief A type-safe union.
+/// @tparam ...Types the types that the Variant is able to hold
+template <typename... Types>
+using Variant = std::variant<Types...>;
+
+/// @brief Exchange the state of lhs with that of rhs.
+/// @tparam ...Types the types that the Variant is able to hold
+/// @param lhs the first Variant
+/// @param rhs the second Variant
+template <typename... Types>
+void swap(Variant<Types...>& lhs, Variant<Types...> rhs) {
+  lhs.swap(rhs);
+}
+}  // namespace ara::core
+
+#endif
