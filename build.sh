2f4285685af82b603af4e9747bf2e091991474f4
杨天幸 <tianxingyang@users.noreply.github.com>
2024-07-21 02:20:48 +0000

    chore: Update build.sh to support clean build and specify build type

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-conan install -of=third-party --build=missing --profile=ap_debug .
+
+if [ $# -eq 1 ] && [ $1 = "clean" ]; then
+    echo "Cleaning build directory..."
+    rm -rf build
+    exit 0
+fi
+
+#build_type="Debug"
+build_type="Release"
+
+conan install -of=third-party --build=missing --profile=./ap_$build_type .
 
 if [ ! -d build ]; then
     mkdir build
 fi
 
 cd build
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_TOOLCHAIN_FILE=x86_64_toolchain ..
-cmake --build . -j16
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE="$build_type" -DCMAKE_TOOLCHAIN_FILE=x86_64_toolchain ..
+cmake --build . -j$(nproc)
